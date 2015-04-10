eeb0a93ab570aca637348a0c3bad9b2a00d6e4f7
mininao <maxence_aici@yahoo.fr>
2015-04-10 18:03:48 +0200

    Beautiful colors

---
+++ bash/beautiful-colors.reset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# todo add correct $device detection
+xrandr --output $DEVICE --gamma 1:1:1
---
+++ bash/beautiful-colors.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# todo add correct $device detection
+xrandr --output $DEVICE --gamma 3:1:4
