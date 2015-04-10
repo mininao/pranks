5b7a9bf71139a48fe2b8688e3ebb143eb1572b9a
Minijackson <minijackson@riseup.net>
2015-04-10 18:15:24 +0200

    Doing TODO of beatiful-colors (-_-)"

--- bash/beautiful-colors.reset.sh
+++ bash/beautiful-colors.reset.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# todo add correct $device detection
+
+# Get the name and put it in the variable "DEVICES"
+# See the file "upside-down.sh"
+DEVICE=$(xrandr -q | awk '/primary/ {print $1; exit;}')
+
 xrandr --output $DEVICE --gamma 1:1:1
